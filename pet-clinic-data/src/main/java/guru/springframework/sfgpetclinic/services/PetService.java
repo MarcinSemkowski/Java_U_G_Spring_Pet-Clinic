ef27103404ea328ffcea8660164bbd155ea1f602
Marcin <marcin96semkowski@gmail.com>
2019-07-23 18:37:35 +0200

    Added Pets and Contact info to owners on startup

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,9 +1,10 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
-
+@Service
 public interface PetService extends CrudService<Pet,Long> {
 
 
