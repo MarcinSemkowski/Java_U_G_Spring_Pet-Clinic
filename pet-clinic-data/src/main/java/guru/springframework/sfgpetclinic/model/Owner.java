ff32b65251bfc8fec5a5bf469189078545106272
Marcin <marcin96semkowski@gmail.com>
2019-08-08 14:43:29 +0200

    added tests and fixing bug

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -13,9 +13,9 @@ import java.util.Set;
 public class Owner extends Person {
 
     @Builder
-    public Owner(String firstName, String lastName, String address,
+    public Owner(Long id,String firstName, String lastName, String address,
                  String city, String telephone, Set<Pet> pets) {
-        super(firstName, lastName);
+        super(id,firstName,lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -71,8 +71,13 @@ public class OwnerMapService extends AbstractMapService<Owner,Long> implements O
     }
 
     @Override
-    public Owner findByLastName(String name) {
-        return null;
+    public Owner findByLastName(String lastName) {
+
+        return this.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 
 
